ed8c53062a27a66bc95881c8dcb6bd4f06af7765
Sayem Chowdhury <sayem314@gmail.com>
2023-07-10 14:01:09 +0600

    Revert "refactor keepawake"
    This reverts commit 5a22a8f26fc37aee54ebea0a1167b0710c8d3635.

--- NativeKCKeepAwake.js
+++
@@ -1,7 +0,0 @@
-import { TurboModuleRegistry } from "react-native";
-
-const ReactNativeKCKeepAwake = TurboModuleRegistry.getEnforcing(
-  "ReactNativeKCKeepAwake"
-);
-
-export default ReactNativeKCKeepAwake;
