375cd2ac8d723602b30bb21c7ac7d6850139d9a2
Alex Zaharia <alex.zaharia@mcro.tech>
2025-05-20 15:38:06 +0300

    fix(core): manage multiple keep-awake instances
    - Implemented tag management to prevent multiple activations/deactivations
    - Updated useKeepAwake and default export to utilize tagId

--- index.native.js
+++ index.native.js
@@ -1,26 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useId } from "react";
 
 import ReactNativeKCKeepAwake from "./NativeKCKeepAwake";
 
-export const activateKeepAwake = () => {
-  ReactNativeKCKeepAwake.activate();
+let tags = [];
+
+export const activateKeepAwake = (tagId) => {
+  if (tags.length <= 0) {
+    ReactNativeKCKeepAwake.activate();
+  }
+  tags.push(tagId);
 };
 
-export const deactivateKeepAwake = () => {
-  ReactNativeKCKeepAwake.deactivate();
+export const deactivateKeepAwake = (tagId) => {
+  tags = tags.filter((id) => id !== tagId);
+  if (tags.length <= 0) {
+    ReactNativeKCKeepAwake.deactivate();
+  }
 };
 
 export const useKeepAwake = () => {
+  const tagId = useId();
   useEffect(() => {
-    activateKeepAwake();
-    return deactivateKeepAwake;
+    activateKeepAwake(tagId);
+    return () => deactivateKeepAwake(tagId);
   }, []);
 };
 
 export default () => {
+  const tagId = useId();
   useEffect(() => {
-    activateKeepAwake();
-    return deactivateKeepAwake;
+    activateKeepAwake(tagId);
+    return () => deactivateKeepAwake(tagId);
   }, []);
 
   return null;
